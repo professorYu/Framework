7560cf07bdd35dd564bcc9d44f699f80a6d8e82a
professorYu <13000590@qq.com>
2019-12-14 18:49:49 +0800

    修改框架生成逻辑

--- UI/Editor/UICodeGenerator.cs
+++ UI/Editor/UICodeGenerator.cs
@@ -1,15 +1,22 @@
 ﻿
+using System;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Reflection;
 using System.Text;
+using UnityEditor.Callbacks;
+using Object = UnityEngine.Object;
 
 public class UICodeGenerator
 {
+    private static string UIMainTemplatePath = Application.dataPath + "/Framework/UI/ConfigData/UIMainTemplate.txt";
+    private static string UIReferenceTemplatePath = Application.dataPath + "/Framework/UI/ConfigData/UIReferenceTemplate.txt";
+    private static string UIScriptSaveFolider = Application.dataPath + "/";
+
     [MenuItem("Assets/生成UI代码")]
     public static void CreateUICode()
     {
-        //mScriptKitInfo = null;
         var objs = Selection.GetFiltered(typeof(GameObject), SelectionMode.Assets | SelectionMode.TopLevel);
         EditorUtility.DisplayProgressBar("", "生成UI代码中...", 0);
         for (var i = 0; i < objs.Length; i++)
@@ -17,51 +24,102 @@ public class UICodeGenerator
             CreateCode(objs[i] as GameObject, AssetDatabase.GetAssetPath(objs[i]));
             EditorUtility.DisplayProgressBar("", "生成UI代码中...", (float)(i + 1) / objs.Length);
         }
-
         EditorUtility.ClearProgressBar();
+
         AssetDatabase.Refresh();
     }
 
-    private static void CreateCode(GameObject obj, string uiPrefabPath)
+    //写引用文件
+    private static void WriteReferenceCode(GameObject clone, string componentName)
     {
-        if (obj != null)
+        UIMark[] marks = clone.transform.GetComponentsInChildren<UIMark>();
+        string referenceStr = "";
+
+        for (int i = 0; i < marks.Length; i++)
         {
-            //PrefabInstanceStatus status = PrefabUtility.GetPrefabInstanceStatus(obj);
-            //if (status == PrefabInstanceStatus.NotAPrefab)
-            //{
-            //    return;
-            //}
-            string name = obj.name;
-            var clone = PrefabUtility.InstantiatePrefab(obj) as GameObject;
-            if (null == clone)
-            {
-                return;
-            }
-
-            Debug.LogError(Application.dataPath);
-
-            string writePath = Application.dataPath + "/" + name + ".Reference.cs";
-
-
-            UIMark[] marks = clone.transform.GetComponentsInChildren<UIMark>();
-            string referenceStr = "";
-
-            for (int i = 0; i < marks.Length; i++)
-            {
-                referenceStr += "public GameObject " + marks[i].name + ";\n";
-                
-            }
-
-            string template = File.ReadAllText(Application.dataPath + "/Framework/UI/ConfigData/" + "UICodeTemplate.txt");
-            template = template.Replace("[ClassName]", name);
-            template = template.Replace("[Reference]", referenceStr);
-
-            File.WriteAllText(writePath, template, Encoding.UTF8);
-            Object.DestroyImmediate(clone);
+            referenceStr += "public GameObject " + marks[i].name + ";\n\t";
         }
 
+        string template = File.ReadAllText(UIReferenceTemplatePath);
+        template = template.Replace("[ClassName]", componentName);
+        template = template.Replace("[Reference]", referenceStr);
 
+        File.WriteAllText(UIScriptSaveFolider + componentName + ".Reference.cs", template, Encoding.UTF8);
+    }
 
+    //写主文件
+    private static void WriteMainCode(string componentName)
+    {
+        string mainCodePath = UIScriptSaveFolider + componentName + ".cs";
+        if (!File.Exists(mainCodePath))
+        {
+            string template = File.ReadAllText(UIMainTemplatePath);
+            template = template.Replace("[ClassName]", componentName);
+            File.WriteAllText(UIScriptSaveFolider + componentName + ".cs", template, Encoding.UTF8);
+        }
+    }
+
+    
+
+    private static void CreateCode(GameObject obj, string uiPrefabPath)
+    {
+        if (obj == null) return;
+
+        PlayerPrefs.SetString("_LOCAL_TEST", uiPrefabPath);
+        string componentName = obj.name;
+        //var clone = PrefabUtility.InstantiatePrefab(obj) as GameObject;
+
+        WriteReferenceCode(obj, componentName);
+        WriteMainCode(componentName);
+
+        //Object.DestroyImmediate(clone);
+    }
+
+    [DidReloadScripts]
+    static void AddComponent2GameObject()
+    {
+        return;
+        Debug.LogError("编译完成");
+
+        string uiPrefabPath = PlayerPrefs.GetString("_LOCAL_TEST");
+        GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(uiPrefabPath);
+        var clone = PrefabUtility.InstantiatePrefab(obj) as GameObject;
+
+        string componentName = obj.name;
+        Component component = clone.GetComponent(componentName);
+
+        if (component == null)
+        {
+            Assembly assembly = AppDomain.CurrentDomain.Load("Assembly-CSharp");
+            Type type = assembly.GetType(componentName);
+            if (type == null)
+            {
+                string template = File.ReadAllText(UIMainTemplatePath);
+                template = template.Replace("[ClassName]", componentName);
+                File.WriteAllText(UIScriptSaveFolider + componentName + ".cs", template, Encoding.UTF8);
+                AssetDatabase.Refresh();
+                type = assembly.GetType(componentName);
+            }
+
+            component = clone.AddComponent(type);
+        }
+
+        FieldInfo[] fileFieldInfos = component.GetType().GetFields();
+        UIMark[] marks = clone.transform.GetComponentsInChildren<UIMark>();
+        foreach (FieldInfo fileFieldInfo in fileFieldInfos)
+        {
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i].name == fileFieldInfo.Name)
+                {
+                    fileFieldInfo.SetValue(component, marks[i].gameObject);
+                    break;
+                }
+            }
+        }
+        PrefabUtility.SaveAsPrefabAssetAndConnect(clone, uiPrefabPath, InteractionMode.AutomatedAction);
+        Object.DestroyImmediate(clone);
+        //return component;
 
     }
 }
