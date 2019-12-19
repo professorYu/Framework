821c70d0222125a4ea52baa7bef82777682628e1
professorYu <13000590@qq.com>
2019-12-19 21:30:52 +0800

    上传启动逻辑

---
+++ GameLaunch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 游戏启动脚本   初始化一切 
+/// </summary>
+public class GameLaunch
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void OnLoadBeforeScene()
+    {
+        Application.targetFrameRate = 60;
+        AudioManager.Instance.Startup();
+        UIManager.Instance.Startup();
+    }
+}
