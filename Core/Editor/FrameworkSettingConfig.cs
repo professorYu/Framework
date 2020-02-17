c632c1b0a297a2d22a908cb0e803e9aca05b6b38
Yz <13000590@qq.com>
2020-02-17 09:51:19 +0800

    序列化UI脚本路径字段

--- Core/Editor/FrameworkSettingConfig.cs
+++ Core/Editor/FrameworkSettingConfig.cs
@@ -6,6 +6,7 @@
 public class FrameworkSettingConfig : ScriptableObject
 {
     //UI代码生成文件夹  通过 FrameworkSetting窗口设置  如果文件夹不存在  则使用Asset文件夹作为目录
+    [SerializeField]
     public string UIScriptGenFolderPath;
 
 }
